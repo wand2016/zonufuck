588b9833e818ea5c3bbb8bc49c26947f246ba021
Daiki Horiyama <d19921207@gmail.com>
2019-12-11 23:45:01 +0900

    ドライバとなるテストクラス作成
    Hello World green test

---
+++ tests/functions/AhiTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace zonuexe\Zonufuck\functions;
+
+use function zonuexe\Zonufuck\gen;
+use function zonuexe\Zonufuck\stream_for;
+use function zonuexe\Zonufuck\strip_line_comment;
+
+/**
+ * Test case for function zonuexe\Zonufuck\gen()
+ *
+ * This test cases are based kinaba's brainfuck article that licensed under NYSL.
+ *
+ * @see http://www.kmonos.net/alang/etc/brainfuck.php
+ * @copyright 2019 zonuexe
+ * @author USAMI Kenta <tadsan@zonu.me>
+ * @license GPL-3.0-or-later
+ */
+class AhiTest extends \zonuexe\Zonufuck\TestCase
+{
+    private const BF_TOKENS = [
+        'inc' => '+',
+        'dec' => '-',
+        'nxt' => '>',
+        'prv' => '<',
+        'opn' => '[',
+        'cls' => ']',
+        'put' => '.',
+        'get' => ',',
+    ];
+
+
+    /**
+     * @dataProvider for_test
+     */
+    public function test(string $expected, string $code, string $input = '')
+    {
+        $output_resource = stream_for('', 'rw');
+        $input_resource = stream_for($input, 'rw');
+
+        $code = strip_line_comment('#', $code);
+
+        $bf = gen(self::BF_TOKENS);
+        $bf($code, $input_resource, $output_resource);
+
+        \rewind($output_resource);
+
+        $this->assertEquals($expected, \stream_get_contents($output_resource));
+    }
+
+    public function for_test()
+    {
+        yield 'matsuahi' => [
+            'Hello World!' . PHP_EOL,
+            '>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++
+++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>
+++++++++[<++++>-]<+.[-]++++++++++.',
+        ];
+    }
+}
